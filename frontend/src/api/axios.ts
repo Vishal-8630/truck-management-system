d4207edd5a06392f15e803cf250bfd0a5d5b657c
Vishal Chaudhary <vishh8630@gmail.com>
2025-10-14 17:38:50 +0530

    Fixed axios api url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// For production: https://divyanshiroadlines-backend.onrender.com/api
+// For production: https://divyanshi-roadlines.onrender.com/api
 // For development: http://localhost:3000/api
 const api = axios.create({
-    baseURL: "https://divyanshiroadlines-backend.onrender.com/api",
+    baseURL: "https://divyanshi-roadlines.onrender.com/api",
     withCredentials: true, // send cookies with requests
     headers: {
         "Content-Type": "application/json",
