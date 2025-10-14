606d9d2cb3eef8e7156b7186d5d13ef7c4d25a91
Vishal Chaudhary <vishh8630@gmail.com>
2025-10-14 17:24:14 +0530

    Added code for cors

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,11 @@ const __dirname = path.dirname(__filename);
 
 // ✅ Allow CORS for dev & prod
 const corsOptions = {
-  origin: process.env.NODE_ENV === "production"
-    ? "*" // or your frontend domain if hosted separately
-    : "http://localhost:5173",
+  origin: [
+    "https://divyanshi-roadlines.onrender.com",
+    "http://localhost:5173",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
@@ -47,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/dist");
   app.use(express.static(frontendPath));
 
-  app.get((req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(frontendPath, "index.html"));
   });
 }
