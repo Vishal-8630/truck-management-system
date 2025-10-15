cb5db892ff6bee2b2900823f569228944723e234
Vishal Chaudhary <vishh8630@gmail.com>
2025-10-15 23:26:07 +0530

    Added code in App.tsx for debugging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,15 +8,19 @@ import useAuthCheck from "./hooks/useAuthCheck";
 import Loading from "./components/Loading";
 import MoveToTopButton from "./components/MoveToTopButton";
 
-import '@syncfusion/ej2-base/styles/material.css';
-import '@syncfusion/ej2-react-calendars/styles/material.css';
+import "@syncfusion/ej2-base/styles/material.css";
+import "@syncfusion/ej2-react-calendars/styles/material.css";
 import { registerLicense } from "@syncfusion/ej2-base";
 
-registerLicense("Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCd0x3R3xbf1x1ZFRGalxTTndYUiweQnxTdEBjUH9bcXRVRGFbVUVwV0leYg==");
-
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCd0x3R3xbf1x1ZFRGalxTTndYUiweQnxTdEBjUH9bcXRVRGFbVUVwV0leYg=="
+);
 
 function App() {
   const checking = useAuthCheck();
+
+  console.log("App Env:", import.meta.env.VITE_APP_ENV);
+  console.log("Base URL:", import.meta.env.VITE_PROD_BASE_URL);
 
   if (checking) return <Loading />;
 
