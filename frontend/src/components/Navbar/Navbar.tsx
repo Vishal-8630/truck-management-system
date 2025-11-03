b752c872ed7b96209a2c13478538e9f6940b892e
Vishal Chaudhary <vishh8630@gmail.com>
2025-11-03 13:28:46 +0530

    Fixed navbar issue

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -9,14 +9,12 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import NavButton from "../NavButton/NavButton";
 import { FaChevronDown } from "react-icons/fa";
-import DRL from '../../assets/drl.png';
+import DRL from "../../assets/drl.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.auth.user);
-  const [show, setShow] = useState(true);
-  const [lastScroll, setLastScroll] = useState(0);
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -26,16 +24,6 @@ const Navbar = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScroll = window.scrollY;
-      setShow(currentScroll < lastScroll || currentScroll < 50);
-      setLastScroll(currentScroll);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScroll]);
 
   const handleLogout = async () => {
     try {
@@ -61,14 +49,12 @@ const Navbar = () => {
               {
                 to: "/bill-entry/new-entry",
                 label: "New Bill Entry",
-                content:
-                  "Click here to create new Billing Entry.",
+                content: "Click here to create new Billing Entry.",
               },
               {
                 to: "/bill-entry/all-bill-entries",
                 label: "All Bill Entries",
-                content:
-                  "Click here to view all Billing Entries.",
+                content: "Click here to view all Billing Entries.",
               },
               {
                 to: "/bill-entry/lrcopy",
@@ -83,8 +69,7 @@ const Navbar = () => {
               {
                 to: "/bill-entry/billing-party",
                 label: "Billing Party",
-                content:
-                  "Click here to add Billing Party.",
+                content: "Click here to add Billing Party.",
               },
             ],
           },
@@ -100,8 +85,7 @@ const Navbar = () => {
               {
                 to: "/vehicle-entry/all-vehicle-entries",
                 label: "All Vehicle Entries",
-                content:
-                  "Click here to view all Vehicle Entries.",
+                content: "Click here to view all Vehicle Entries.",
               },
               {
                 to: "/vehicle-entry/new-balance-party",
@@ -132,8 +116,7 @@ const Navbar = () => {
               {
                 to: "/journey/all-journey-entries",
                 label: "All Journey Entries",
-                content:
-                  "Click here to view all Journey Entries.",
+                content: "Click here to view all Journey Entries.",
               },
               {
                 to: "/journey/new-truck-entry",
@@ -166,18 +149,14 @@ const Navbar = () => {
 
   return (
     <div className={styles.navbar}>
-      <motion.nav
-        initial={{ opacity: 0, y: 0 }}
-        animate={{ opacity: show ? 1 : 0, y: show ? 0 : -120 }}
-        transition={{ duration: 0.4, ease: "easeInOut" }}
-      >
+      <nav>
         <div className={styles.brand}>
           <img src={DRL} alt="DRL" />
         </div>
         <motion.ul
           className={styles.links}
           variants={{
-            hidden: { opacity: isMobile ? 0 : 1, y: isMobile ? -500 : 0 },
+            hidden: { opacity: isMobile ? 0 : 1, y: isMobile ? -800 : 0 },
             visible: { opacity: 1, y: 0 },
           }}
           initial="hidden"
@@ -209,7 +188,7 @@ const Navbar = () => {
             animate={isNavOpen ? { rotate: -45, y: -8 } : { rotate: 0, y: 0 }}
           ></motion.span>
         </div>
-      </motion.nav>
+      </nav>
     </div>
   );
 };
